e11c7ae4573f01ca272526da03be84af3090d5e0
Antonio Feitosa <antonio.cfc@gmail.com>
2014-05-21 13:07:41 -0300

    added extraction archive to buildir

--- Formulas/ircii/Formula.pm
+++ Formulas/ircii/Formula.pm
@@ -2,35 +2,36 @@
 package Formula;
 
 sub new {
-	my $class = shift;
-	my $prefix = shift;
-	my $self = {};
-	bless( $self, $class );
-	$self->{prefix}		= $prefix;
+    my $class  = shift;
+    my $prefix = shift;
+    my $self   = {};
+    bless( $self, $class );
+    $self->{prefix} = $prefix;
 
-	$self->{name}		= "ircii";
-	$self->{homepage}	= "http://www.eterna.com.au/ircii/";
-	$self->{url}		= "http://ircii.warped.com/ircii-20131230.tar.bz2";
-	$self->{sha1}		= "b59f8fcd344c09b8820f6efbcc74ba9af7e587ce";
-	$self->{type}		= "package";
-	$self->{depends}	= qw(gmake);
-	$self->{make}		= "make";
+    $self->{name}     = "ircii";
+    $self->{homepage} = "http://www.eterna.com.au/ircii/";
+    $self->{url}      = "http://ircii.warped.com/ircii-20131230.tar.bz2";
+    $self->{sha1}     = "b59f8fcd344c09b8820f6efbcc74ba9af7e587ce";
+    $self->{type}     = "package";
+    $self->{depends}  = qw(gmake);
+    $self->{make}     = "make";
 
-	return $self;
+    return $self;
 }
 
 sub install {
-	my $self = shift;
-	my $install = shift;
-	my $make = $self->{make};
-	system("./configure --prefix $self->{prefix}",
-							"--disable-debug",
-							"--enable-ipv6",
-							"--disable-dependency-tracking",
-							"--with-default=irc.freenode.net"
-	);
-	system("$make");
-	system("$make install");
+    my $self    = shift;
+    my $install = shift;
+    my $make    = $self->{make};
+    system(
+        "./configure --prefix $self->{prefix}",
+        "--disable-debug",
+        "--enable-ipv6",
+        "--disable-dependency-tracking",
+        "--with-default=irc.freenode.net"
+    );
+    system("$make");
+    system("$make install");
 }
 
 1;
--- Formulas/nano/Formula.pm
+++ Formulas/nano/Formula.pm
@@ -2,32 +2,30 @@
 package Formula;
 
 sub new {
-	my $class = shift;
-	my $prefix = shift;
-	my $self = {};
-	bless( $self, $class );
-	
-	$self->{prefix}		= $prefix;
-	$self->{name}		= "nano";
-	$self->{homepage}	= "http://www.nano-editor.org/";
-	$self->{url}		= "http://www.nano-editor.org/dist/v2.2/nano-2.2.6.tar.gz";
-	$self->{sha1}		= "f2a628394f8dda1b9f28c7e7b89ccb9a6dbd302a";
-	$self->{type}		= "package";
-	$self->{depends}	= qw(gmake);
-	$self->{make}		= "make";
+    my $class  = shift;
+    my $prefix = shift;
+    my $self   = {};
+    bless( $self, $class );
 
-	return $self;
+    $self->{prefix}   = $prefix;
+    $self->{name}     = "nano";
+    $self->{homepage} = "http://www.nano-editor.org/";
+    $self->{url}     = "http://www.nano-editor.org/dist/v2.2/nano-2.2.6.tar.gz";
+    $self->{sha1}    = "f2a628394f8dda1b9f28c7e7b89ccb9a6dbd302a";
+    $self->{type}    = "package";
+    $self->{depends} = qw(gmake);
+    $self->{make}    = "make";
+
+    return $self;
 }
 
 sub install {
-	my $self = shift;
-	my $install = shift;
-	my $make = $self->{make};
-	system("./configure --prefix $self->{prefix}",
-							"--enable-utf8"
-	);
-	system("$make");
-	system("$make install");
+    my $self    = shift;
+    my $install = shift;
+    my $make    = $self->{make};
+    system( "./configure --prefix $self->{prefix}", "--enable-utf8" );
+    system("$make");
+    system("$make install");
 }
 
 1;
